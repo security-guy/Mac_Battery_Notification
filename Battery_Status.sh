ad73d209d485154006b53ea1faff26022d22cb33
security-guy <48224905+security-guy@users.noreply.github.com>
2020-05-28 19:54:36 -0400

    Create Battery_Status.sh

---
+++ Battery_Status.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# gets battery level
+Keyboard=`ioreg -c AppleBluetoothHIDKeyboard |grep '"BatteryPercent" =' | cut -d= -f2 | cut -d ' ' -f 2`
+Mouse=`ioreg -c BNBMouseDevice |grep '"BatteryPercent" =' | cut -d= -f2 | cut -d ' ' -f 2`
+
+
+#battery level threshold
+power_level=10
+
+if [ "$Keyboard" -lt "$power_level" ]; then
+	osascript -e "display notification \"Keyboard battery is at ${Keyboard}%.\" with title \"Keyboard Battery Low\""
+fi
+
+
+if [ "$Mouse" -lt "$power_level" ]; then
+	osascript -e "display notification \"Mouse battery is at ${Mouse}%.\" with title \"Mouse Battery Low\""
+fi
