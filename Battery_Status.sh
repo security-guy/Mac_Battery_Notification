3f8c3f5092a6d2dca67cae74dced8ffc7a3bc608
security-guy <48224905+security-guy@users.noreply.github.com>
2020-05-28 20:29:53 -0400

    Update Battery_Status.sh

--- Battery_Status.sh
+++ Battery_Status.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
 # gets battery level
-Keyboard=`ioreg -c AppleBluetoothHIDKeyboard |grep '"BatteryPercent" =' | cut -d= -f2 | cut -d ' ' -f 2`
-Mouse=`ioreg -c BNBMouseDevice |grep '"BatteryPercent" =' | cut -d= -f2 | cut -d ' ' -f 2`
+Keyboard1=`ioreg -c AppleBluetoothHIDKeyboard |grep '"BatteryPercent" =' | cut -d= -f2 | cut -d ' ' -f 2`
+Keyboard2=`ioreg -c IOAppleBluetoothHIDDriver |grep '"BatteryPercent" =' | cut -d= -f2 | cut -d ' ' -f 2`
+
+Mouse1=`ioreg -c BNBMouseDevice |grep '"BatteryPercent" =' | cut -d= -f2 | cut -d ' ' -f 2`
+Mouse2=`ioreg -c AppleHSBluetoothDevice |grep '"BatteryPercent" =' | cut -d= -f2 | cut -d ' ' -f 2`
 
 
-#battery level threshold
+#battery level variable
 power_level=10
 
-if [ "$Keyboard" -lt "$power_level" ]; then
-	osascript -e "display notification \"Keyboard battery is at ${Keyboard}%.\" with title \"Keyboard Battery Low\""
+if [ "$Keyboard1" -lt "$power_level" ]; then
+	osascript -e "display notification \"Keyboard battery is at ${Keyboard1}%.\" with title \"Keyboard Battery Low\""
 fi
 
+if [ "$Keyboard2" -lt "$power_level" ]; then
+	osascript -e "display notification \"Keyboard battery is at ${Keyboard2}%.\" with title \"Keyboard Battery Low\""
+fi
 
-if [ "$Mouse" -lt "$power_level" ]; then
-	osascript -e "display notification \"Mouse battery is at ${Mouse}%.\" with title \"Mouse Battery Low\""
+if [ "$Mouse1" -lt "$power_level" ]; then
+	osascript -e "display notification \"Mouse battery is at ${Mouse1}%.\" with title \"Mouse Battery Low\""
+fi
+
+if [ "$Mouse2" -lt "$power_level" ]; then
+	osascript -e "display notification \"Mouse battery is at ${Mouse2}%.\" with title \"Mouse Battery Low\""
 fi
