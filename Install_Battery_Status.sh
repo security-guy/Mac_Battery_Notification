182ca7751d08f6bb9c39c6536477ac582fa2a1f1
security-guy <48224905+security-guy@users.noreply.github.com>
2020-06-03 16:24:13 -0400

    Update Install_Battery_Status.sh

--- Install_Battery_Status.sh
+++ Install_Battery_Status.sh
@@ -8,4 +8,4 @@ echo "[+] Moving file to /usr/local/bin PATH"
 mv Battery_Status.sh /usr/local/bin/
 
 echo "[+] Creating Cronjob to run the Battery_Status.sh script every 30 minutes"
-cat <(crontab -l) <(echo "PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin" && echo "*/30 * * * * battery_checker.sh") | crontab -
+cat <(crontab -l) <(echo "PATH=/usr/local/bin:/usr/local/sbin:~/bin:/usr/bin:/bin:/usr/sbin:/sbin" && echo "*/30 * * * * Battery_Status.sh") | crontab -
